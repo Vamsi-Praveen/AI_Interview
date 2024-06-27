ac86e0cd700a44fe020a52939cd17acec84b577a
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2024-06-27 20:11:26 +0530

    changed to post method

--- server/routes/feedback.route.js
+++ server/routes/feedback.route.js
@@ -4,6 +4,6 @@ import { verifyAnswer } from "../controllers/feedback.controller.js";
 
 const feedBackRouter = express.Router();
 
-feedBackRouter.put('verify-answer',verifyAuth,verifyAnswer)
+feedBackRouter.post('verify-answer',verifyAuth,verifyAnswer)
 
 export default feedBackRouter
